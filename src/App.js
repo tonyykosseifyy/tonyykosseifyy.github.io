97da644c6d5890068df2c6be616f782ab7e5c35e
Tony Kosseify <tonykosseify123@gmail.com>
2022-12-23 15:02:28 +0200

    pages animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react'
+import "./App.css";
+import Navbar from './components/Navbar';
+import ViewPager from './components/ViewPager';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+const App = () => {
+    const [currentFace, setCurrentFace] = useState("front");
+    const handleButtonClick = (newFace) => {
+      setCurrentFace(newFace);
+    }
+    return (
+      <section>
+        <ViewPager currentFace={currentFace}>
+          {/*<Navbar handleButtonClick={(newFace) => handleButtonClick(newFace)}/>*/}
+        </ViewPager>
+      </section>
+    );
+};
+
+export default App ;
