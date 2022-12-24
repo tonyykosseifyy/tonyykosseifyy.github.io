2c6c820e5568913580cea2d4677f91e4d8aed16b
Tony Kosseify <tonykosseify123@gmail.com>
2022-12-24 18:35:53 +0200

    starting with the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import "../PageStyles/About.css";
+
 
 function About() {
   return (
-    <div>About</div>
-  )
-}
+    <div className='about'>
+      <h1 className='heading'>A little about me</h1>
+      <div className='about_me'>
+        <p>
+          When I first became passionate about coding, I was 15 years old. I came across a python video while browsing YouTube and thought, "Why not let's try it!"
+          And here I am passionate about programming and problem solving . 
+          After a little research about python I enrolled in a competition held in LAU 
+          against some experienced programmers and got third place. And from there the journey 
+          had begun . My interests shifted to web development so I started learning Html, css,
+          javascript, react, redux . Then developing my backend skills with node, express 
+          and mongoDB . I built many successfull full stack websites before getting involved 
+          in the Mobile App development field . Currently , I am first year student at the
+          Lebanese American University studying computer science and eager to learn more!
+        </p>
+      </div>
+      
+    </div>
+  );
+};
 
-export default About
+export default About;
