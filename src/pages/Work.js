38830b245e2088974157ba020d4ee84374cdf7d7
Tony Kosseify <tonykosseify123@gmail.com>
2022-12-29 22:07:45 +0200

    add links to projects

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -28,56 +28,56 @@ function Work() {
           <div className='project'>
             <img src={require("../components/assets/netflix.png")} alt="Netflix" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="http://netflix-clone-b32fc.web.app" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/netflix-clone" target="_blank">check github</a>
             </div>
           </div>
           
           <div className='project'>
             <img src={require("../components/assets/amazon-clone.png")} alt="Amazon" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="http://react-auth-b5a1f.web.app" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/amazon-clone" target="_blank">check github</a>
             </div>
           </div>
           
           <div className='project'>
             <img src={require("../components/assets/tesla-clone.png")} alt="Tesla" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="https://tesla-clone-cbdc1.web.app/" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/tesla-clone" target="_blank">check github</a>
             </div>
           </div>
 
           <div className='project'>
             <img src={require("../components/assets/coffee-shop.png")} alt="Coffee-shop" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="https://coffee-shop-clone-3968d.web.app/" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/coffee-shop-clone" target="_blank">check github</a>
             </div>
           </div>
 
           <div className='project'>
             <img src={require("../components/assets/easy-bank.png")} alt="easy-bank" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="http://easy-bank-a54d3.web.app" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/easy-bank-project" target="_blank">check github</a>
             </div>
           </div>
           
           <div className='project'>
             <img src={require("../components/assets/ip-address.png")} alt="ip-address-project" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="https://ip-address-project-85ce4.web.app/" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/Ip-Address-Project" target="_blank">check github</a>
             </div>
           </div>
 
           <div className='project'>
             <img src={require("../components/assets/tip-calculator.png")} alt="tip-calculator" />
             <div>
-              <a href="/" target="_blank">check website</a>
-              <a href="/" target="_blank">check github</a>
+              <a href="https://tip-calculator-e0013.firebaseapp.com/" target="_blank">check website</a>
+              <a href="https://github.com/tonyykosseifyy/Tip-Calculator-Project" target="_blank">check github</a>
             </div>
           </div>
 
@@ -89,7 +89,7 @@ function Work() {
               <img src="https://raw.githubusercontent.com/selimellieh72/MetaMedApp/main/preview/transcripts.png?token=GHSAT0AAAAAAB4VAEIYWK4RQ73X55UFY5SGY5N7X6A" alt="mobile app" />
             </div>
             <div>
-              <a href="/" target="_blank">check github</a>
+              <a href="https://github.com/selimellieh72/MetaMedApp" target="_blank">check github</a>
             </div>
           </div>
         </div>
